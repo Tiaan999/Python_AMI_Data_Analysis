225af678fe031b8e6f9c95e869fc1950890d10ba
Tiaan999 <tiaan1.willemse@gmail.com>
2016-03-04 14:57:01 +0200

    Changed filename - add_to_db_ami.py

--- add_to_db_ami.py
+++ add_to_db_ami.py
@@ -1,35 +1,39 @@
 import pymongo
+import datetime as dt
 from datetime import datetime
 import os
 import xlrd
 
 main_start = datetime.now()
 db = pymongo.MongoClient("mongodb://localhost:27017/")['test']
-time_half_hour = ['00:00','00:30','01:00','01:30','02:00','02:30','03:00','03:30',
-                  '04:00','04:30','05:00','05:30','06:00','06:30','07:00','07:30',
-                  '08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30',
-                  '12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30',
-                  '16:00','16:30','17:00','17:30','18:00','18:30','19:00','19:30',
-                  '20:00','20:30','21:00','21:30','22:00','22:30','23:00','23:30']
+time_half_hour = ['00:00', '00:30', '01:00', '01:30', '02:00', '02:30', '03:00', '03:30',
+                  '04:00', '04:30', '05:00', '05:30', '06:00', '06:30', '07:00', '07:30',
+                  '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30',
+                  '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
+                  '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30',
+                  '20:00', '20:30', '21:00', '21:30', '22:00', '22:30', '23:00', '23:30']
 
 filepath = open('file_path.txt').read()
-for file in os.listdir(filepath):
-    if file.endswith(".xlsx"):
-        wb = xlrd.open_workbook(filepath + file)
+for file_f in os.listdir(filepath):
+    if file_f.endswith(".xlsx"):
+        wb = xlrd.open_workbook(filepath + file_f)
         if wb.sheet_names()[0] == 'Sheet1':
             start = datetime.now()
             sheet = wb.sheet_by_name('Sheet1')
             number_of_entries = sheet.nrows
             customer = str(sheet.row(1)[0])[7:-2]
             bulk = db[customer].initialize_ordered_bulk_op()
-            for i in range(1,number_of_entries):
-                date = datetime(*xlrd.xldate_as_tuple(sheet.row(i)[2].value,wb.datemode)).strftime('%Y/%m/%d')
-                time = str(time_half_hour.index(datetime(*xlrd.xldate_as_tuple(sheet.row(i)[2].value,wb.datemode)).strftime('%H:%M')) + 1)
+            for i in range(1, number_of_entries):
+                date_time = datetime(1899, 12, 30) + dt.timedelta(days=sheet.row(i)[2].value)
+                date = date_time.strftime('%Y/%m/%d')
+                time = date_time.strftime('%H:%M')
+                time_index = str(time_half_hour.index(time) + 1)
                 value = int(sheet.row(i)[3].value)
-                bulk.find({"date": date}).upsert().update({"$set": {'energy_kwh.' + time: value}})
+                bulk.find({"date": date}).upsert().update({"$set": {'energy_kwh.' + time_index: value}})
             bulk.execute()
             end = datetime.now()
-            print str(number_of_entries) + " entries were added to the DB in " + str((end - start).seconds) + " seconds, from " + file + "."
+            print str(number_of_entries) + " entries were added to the DB in " + str((end - start).seconds) \
+                + " seconds, from " + file_f + "."
             print db[customer].find().skip(db[customer].count() - 1)[0]
         else:
             start = datetime.now()
@@ -37,14 +41,17 @@ for file in os.listdir(filepath):
             number_of_entries = sheet.nrows
             customer = str(sheet.row(0)[0])[-14:-1]
             bulk = db[customer].initialize_ordered_bulk_op()
-            for i in range(10,number_of_entries):
-                date = datetime.strptime(sheet.row(i)[0].value,'%Y/%m/%d %H:%M').strftime('%Y/%m/%d')
-                time = str(time_half_hour.index(datetime.strptime(sheet.row(i)[0].value,'%Y/%m/%d %H:%M').strftime('%H:%M')) + 1)
+            for i in range(10, number_of_entries):
+                date_time = datetime.strptime(sheet.row(i)[0].value, '%Y/%m/%d %H:%M')
+                date = date_time.strftime('%Y/%m/%d')
+                time = date_time.strftime('%H:%M')
+                time_index = str(time_half_hour.index(time) + 1)
                 value = int(sheet.row(i)[1].value)
-                bulk.find({"date": date}).upsert().update({"$set": {'energy_kwh.' + time: value}})
+                bulk.find({"date": date}).upsert().update({"$set": {'energy_kwh.' + time_index: value}})
             bulk.execute()
             end = datetime.now()
-            print str(number_of_entries) + " entries were added to the DB in " + str((end - start).seconds) + " seconds, from x" + file + "."
+            print str(number_of_entries) + " entries were added to the DB in " + str((end - start).seconds) \
+                + " seconds, from x" + file_f + "."
             print db[customer].find().skip(db[customer].count() - 1)[0]
 
 main_end = datetime.now()
--- query_particular_values.py
+++ query_particular_values.py
@@ -1,13 +1,13 @@
 from pymongo import MongoClient
 from datetime import datetime
-from datetime import date
 
 db = MongoClient("mongodb://localhost:27017/")['test']
 collections = db.collection_names()
 for coll in collections:
-    first_date = datetime.strptime(db[coll].find().limit(1)[0]['date'],'%Y/%m/%d').date()
-    last_date = datetime.strptime(db[coll].find().skip(db[coll].count() - 1)[0]['date'],'%Y/%m/%d').date()
+    first_date = datetime.strptime(db[coll].find().limit(1)[0]['date'], '%Y/%m/%d').date()
+    last_date = datetime.strptime(db[coll].find().skip(db[coll].count() - 1)[0]['date'], '%Y/%m/%d').date()
     number_of_days_between_dates = (last_date - first_date).days + 1
     number_of_days_in_record = db[coll].count()
     percent_complete = float(number_of_days_in_record)/float(number_of_days_between_dates)*100
-    print 'Customer: ' + coll + ', First: ' + str(first_date) + ', Last: ' + str(last_date) + ', Percentage complete: ' + str(("{0:.0f}".format(round(percent_complete)))) + '%'
+    print 'Customer: ' + coll + ', First: ' + str(first_date) + ', Last: ' + str(last_date) + ', Percentage complete: '\
+          + str(("{0:.0f}".format(round(percent_complete)))) + '%'
